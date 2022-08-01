0ceb115823f86540cdff551481c30d09a892f74d
ilMateusli <88564848+ilMateusli@users.noreply.github.com>
2022-08-01 10:34:09 -0300

    Update lab.py

--- lab.py
+++ lab.py
@@ -3,7 +3,7 @@ from transformers import AutoModelForCausalLM, AutoTokenizer, set_seed
 import torch
 import re
 
-#torch.set_default_tensor_type(torch.cuda.FloatTensor)
+torch.set_default_tensor_type(torch.cuda.FloatTensor)
 
 #model = AutoModelForCausalLM.from_pretrained("bigscience/bloom-1b3", use_cache=True)
 #tokenizer = AutoTokenizer.from_pretrained("bigscience/bloom-1b3")
