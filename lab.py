8b0245ee58df6948b46663ca61644294bb782ca7
ilMateusli <88564848+ilMateusli@users.noreply.github.com>
2022-08-01 08:59:53 -0300

    Update lab.py

--- lab.py
+++ lab.py
@@ -1,37 +1,44 @@
 import streamlit as st
-import os
-import openai
+from transformers import AutoModelForCausalLM, AutoTokenizer, set_seed
+import torch
 import re
 import urllib.request
 from PIL import Image
 
-openai.api_key = ""
+torch.set_default_tensor_type(torch.cuda.FloatTensor)
+model = AutoModelForCausalLM.from_pretrained("bigscience/bloom-1b3", use_cache=True)
+tokenizer = AutoTokenizer.from_pretrained("bigscience/bloom-1b3")
 
-st.title("Labinftec APP - Detalhar")
-texto = st.text_area("Escreva sintomas causados por algum vírus, ou o nome dele.", "")
-keyopenai = st.text_input("Coloque a chave da OpenAI aqui", "")
+set_seed(424242)
 
-if keyopenai != "":
-    openai.api_key = keyopenai
+model.__class__.__name__
 
-texto_quebrado = texto.replace(',', '\n')
+#prompt = ''
+texto0 = st.text_area("Escreva sintomas causados por algum vírus, ou o nome dele.", "")
+
+input_ids = tokenizer(prompt, return_tensors="pt").to(0)
+
+sample = model.generate(**input_ids, max_length=100, top_k=0, temperature=0.7)
+
+
+
+#print(tokenizer.decode(sample[0], truncate_before_pattern=[r"\n\n^#", "^'''", "\n\n\n"]))
+
+
+
+st.title("Labinftec")
+
+#keyopenai = st.text_input("Coloque a chave da OpenAI aqui", "")
+
+texto_quebrado = texto0.replace(',', '\n')
 if st.button("Detalhar"):
-    if texto == "":
+    if texto0 == "":
         st.write("Você precisa escrever algo na caixa de texto.")
     else: 
-        response = openai.Completion.create(
-  engine="text-davinci-002",
-  prompt="Lista:\n"+texto_quebrado+"\n'''\nFale sobre as características do vírus da lista (nome científico ou a qual família pertence, envelope, diametro, se é RNA ou DNA, sua replicação, informações do genoma, em qual hospedeiro parasita - animais, planta, bacterias, fungos etc, qual sua classe/tamanho), se for sintomas, escolha o vírus que mais se aproxima aos sintomas dados na lista e mostre os motivos da escolha:\n\n Bom.",
-  temperature=0,
-  max_tokens=200,
-  top_p=1,
-  frequency_penalty=0,
-  presence_penalty=0,
-  stop=["\n\n\n"]
-)
-
-        st.write("Detalhes obtidos:")
-        texto = response.choices[0].text
+        
+        prompt = "Lista:\n"+texto_quebrado+"\n'''\nFale sobre as características do vírus da lista (nome científico ou a qual família pertence, envelope, diametro, se é RNA ou DNA, sua replicação, informações do genoma, em qual hospedeiro parasita - animais, planta, bacterias, fungos etc, qual sua classe/tamanho), se for sintomas, escolha o vírus que mais se aproxima aos sintomas dados na lista e mostre os motivos da escolha:
+        texto = tokenizer.decode(sample[0], truncate_before_pattern=[r"\n\n^#", "^'''", "\n\n\n"])
+        t.write("Detalhes obtidos:")
         padrao = re.compile(r'\w+[dae]$')
         familia = padrao.findall(texto)
         link = 'https://www.gettyimages.com.br/fotos/' #-
@@ -46,7 +53,6 @@ if st.button("Detalhar"):
                 if i.startswith('http') and not i.endswith('.gif'):
                     urllib.request.urlretrieve(i, 'imagem.jpg')
                     break
-        texto = response.choices[0].text
         if len(familia) == 0:
             st.write("Não foi possível encontrar uma imagem para mostrar.")
         else:
@@ -55,16 +61,11 @@ if st.button("Detalhar"):
             os.remove('imagem.jpg')
             st.write("Imagem do vírus encontrado pela internet.")
             
-        content = response.choices[0].text
-        explicação0 = openai.Completion.create(
-            engine="text-davinci-002",
-            prompt="Lista:\n"+texto_quebrado+"\n'''\nDescreva os vírus da lista:\n\n"+content+"\nCite vírus relacionado e explique: Além desse,",
-            temperature=0.7,
-            max_tokens=200,
-            top_p=1,
-            frequency_penalty=0,
-            presence_penalty=0,
-            stop=["\n\n"])
-        explicação = explicação0.choices[0].text
+        content = texto
+        explicação0 = "Lista:\n"+texto_quebrado+"\n'''\nDescreva os vírus da lista:\n\n"+content+"\nCite vírus relacionado e explique: Além desse,",
+        input_ids = tokenizer(explicação0, return_tensors="pt").to(0)
+        sample = model.generate(**input_ids, max_length=100, top_k=0, temperature=0.7)
+        
+        explicação = tokenizer.decode(sample[0], truncate_before_pattern=[r"\n\n^#", "^'''", "\n\n\n"])
         st.write(content+".\n\n"+"Além desse,"+explicação)
         st.write("[Você não pode confiar 100% nessa resposta. O programa ainda está em fase de desenvolvimento e pode conter erros.]")
